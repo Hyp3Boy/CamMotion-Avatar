fbcbbcddb93ae283bf62a586d834d8b3e1ca5953
Lenin Chavez <102402199+Hyp3Boy@users.noreply.github.com>
2025-06-03 16:40:54 -0500

    feat: add main menu UI

---
+++ main_ui.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_play_button_pressed() -> void:
+	print("Play")
+
+
+func _on_world_button_pressed() -> void:
+	print("World")
+
+
+func _on_avatar_button_pressed() -> void:
+	print("Avatar")
